115ad01c0a92ad499c09379374654698370818b3
ShravanDeb <shravandeb@gmail.com>
2026-06-30 13:24:00 +0530

    fix: use inline style on scroll-anchor to guarantee spacing above input bar on mobile

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -90,7 +90,7 @@ export default function MessageList({
             )}
           </>
         )}
-        <div ref={bottomRef} className="scroll-anchor" />
+        <div ref={bottomRef} className="scroll-anchor" style={{ height: '200px' }} />
       </div>
 
       {userScrolledUp && (
